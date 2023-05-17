3942653491e3a453e0bc3b5aee8dbc5aabefa517
Hamseapdiasiis <hamse906@gmail.com>
2023-05-17 15:35:01 +0300

    waa exercise oo dhamaystiran

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let A = 23
+let B = 25
+if(A > B){
+    console.log("the largest number out of two numbers is 23")
+}else if(B > A){
+    console.log("the largest number out of two numbers is 25")
+}
